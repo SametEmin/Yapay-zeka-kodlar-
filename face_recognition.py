8b26e3049ecace8d6c19fb5212631313107ace25
SametEmin <115692383+SametEmin@users.noreply.github.com>
2023-08-28 01:52:56 +0300

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -1,35 +1,37 @@
 import cv2
-import face_recognition
+import dlib
 
-# Load a sample image with known faces and their names
-known_image = face_recognition.load_image_file("NBX_Snapshot_2023-07-19_11-37-50-416.png")
-known_face_encoding = face_recognition.face_encodings(known_image)[0]
+# Load a sample image with known faces
+known_image_path = "known_face.jpg"
+known_image = cv2.imread(known_image_path)
 known_face_name = "Known Person"
 
 # Initialize webcam
 video_capture = cv2.VideoCapture(0)
 
+# Load the face detector from dlib
+face_detector = dlib.get_frontal_face_detector()
+
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    # Find all face locations and encodings in the current frame
-    face_locations = face_recognition.face_locations(frame)
-    face_encodings = face_recognition.face_encodings(frame, face_locations)
+    # Convert frame to grayscale for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_detector(gray)
 
     # Iterate through detected faces
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # Compare the face encoding with the known face
-        matches = face_recognition.compare_faces([known_face_encoding], face_encoding)
-        name = "Unknown"
+    for face in faces:
+        x, y, w, h = face.left(), face.top(), face.width(), face.height()
 
-        if True in matches:
-            name = known_face_name
+        # Draw a rectangle around the face
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-        # Draw a rectangle around the face and display the name
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        # Display the name
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+        cv2.putText(frame, known_face_name, (x + 6, y - 6), font, 0.5, (255, 255, 255), 1)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
